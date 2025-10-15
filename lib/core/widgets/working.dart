872c180c4b14ea6002392f79c4da48bc0f2ea1ae
JP Duplessis <jp@vteam.com>
2025-10-15 08:07:20 +0100

    fix WorkingIndicator radius

--- lib/core/widgets/working.dart
+++ lib/core/widgets/working.dart
@@ -7,11 +7,13 @@ class WorkingIndicator extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    // The CupertinoActivityIndicator radius should be size/4 to fit within the bounds
+    // since the indicator has some padding around it
     return Center(
       child: SizedBox(
         height: size,
         width: size,
-        child: CupertinoActivityIndicator(radius: size),
+        child: CupertinoActivityIndicator(radius: size / 2),
       ),
     );
   }
