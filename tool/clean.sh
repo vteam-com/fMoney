75a95aeaae376bcddae755d4896db85adb2a6ea2
JP Duplessis <jp@coachexmachina.com>
2026-04-09 17:15:47 +0100

    migrate from cocoaPod to SwiftPackage

--- tool/clean.sh
+++ tool/clean.sh
@@ -2,6 +2,11 @@
 
 # rm -R ./build
 
+# Remove CocoaPods artifacts
+rm -rf ios/Pods ios/Podfile.lock ios/.symlinks
+# macOS plugins still require CocoaPods (no SPM support yet)
+rm -rf macos/Pods macos/Podfile.lock macos/.symlinks
+
 flutter clean
 flutter pub get
 flutter pub upgrade
--- tool/test.sh
+++ tool/test.sh
@@ -2,11 +2,6 @@
 
 tool/clean.sh
 
-# Prefer Homebrew CocoaPods over potentially broken gem shim binaries.
-if [ -x /opt/homebrew/bin/pod ]; then
-  PATH="/opt/homebrew/bin:$PATH"
-fi
-
 flutter test integration_test --coverage --coverage-path=coverage/lcov_integration.info -d macos --enable-impeller || exit 1
 
 flutter test --coverage --coverage-path=coverage/lcov_units.info || exit 1
